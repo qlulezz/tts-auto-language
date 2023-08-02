1a2bb49eede6ed5968a0892d3ad4930ad43ca690
qlulezz <54777265+qlulezz@users.noreply.github.com>
2023-08-02 18:49:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# TTS-Auto-Language
+
+Text-To-Speech for Twitch, but it switches voices based on the language of the message.<br>
+
+**Currently only supports german and english, but support for more languages are planned.**
+
+## Config
+
+All of the configuration is done in the `config.json` file.<br>
+Variables should be self-explanatory, more configuration will likely be added in the future.
+
+## Run
+
+You need to have Node.js Version 18+ installed, then run `npm start` or `node index.js`<br>
+
+## Build
+
+Run `npm run build` to build the app into a standalone EXE file.
---
+++ index.js
@@ -0,0 +1,97 @@
+const fs = require("fs");
+const tmi = require("tmi.js");
+const say = require("say");
+const LanguageDetect = require("languagedetect");
+
+const lngDetector = new LanguageDetect();
+
+const config = JSON.parse(fs.readFileSync("config.json"));
+say.getInstalledVoices((err, voices) =>
+  console.log("Available voices", voices)
+);
+
+const client = new tmi.Client({
+  options: { debug: true },
+  identity: {
+    username: config.twitch_bot_name,
+    password: config.twitch_oauth,
+  },
+  channels: config.twitch_channel_names,
+});
+
+client.connect().catch(console.error);
+
+const queue = [];
+
+client.on("message", (channel, tags, message, self) => {
+  if (self) return;
+
+  // Ignore messages starting with '!' and '@'
+  if (message.at(0) === "@") {
+    message = message.split(" ").splice(1).join(" ");
+  }
+
+  if (
+    config.ignore_exclamation &&
+    (message.at(0) === "!" || message.at(0) === "@")
+  ) {
+    return;
+  }
+
+  const matches = lngDetector.detect(message);
+  const german = isGerman(matches);
+
+  queue.push({
+    user: replaceName(tags.username),
+    message: message,
+    voice: german ? config.german_voice : config.english_voice,
+  });
+
+  // If the queue was empty before adding this message, start reading immediately
+  if (queue.length === 1) {
+    processQueue();
+  }
+});
+
+let lastUsername = "";
+function processQueue() {
+  // If the queue is empty, stop processing
+  if (queue.length === 0) {
+    return;
+  }
+
+  const { user, message, voice } = queue[0];
+
+  say.speak(
+    config.say_name && config.skip_consecutive_name && lastUsername !== user
+      ? `${user}: ${message}`
+      : message,
+    voice,
+    1.0,
+    (err) => {
+      if (err) {
+        console.error(err);
+      }
+      lastUsername = user;
+      queue.shift();
+      processQueue();
+    }
+  );
+}
+
+function isGerman(matches) {
+  const englishMatch = matches.find((arr) => arr[0] === "english");
+  const germanMatch = matches.find((arr) => arr[0] === "german");
+  try {
+    return englishMatch[1] < germanMatch[1];
+  } catch {
+    return false;
+  }
+}
+
+function replaceName(name) {
+  if (config.name_replacement[name]) {
+    return config.name_replacement[name];
+  }
+  return name;
+}
